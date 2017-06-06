def71333c0a551b7ecf81269ff32302151b09230
Dennis Kleber <dennis_kleber@mailbox.org>
2017-06-06 22:00:32 +0200

    ssl_server

---
+++ app/ssl/ssl_server.py
@@ -0,0 +1,16 @@
+from bottle import Bottle, run, request, server_names, ServerAdapter
+
+class MySSLCherryPy(ServerAdapter):
+    def run(self, handler):
+        from cherrypy import _cpwsgiserver3
+        server = _cpwsgiserver3.CherryPyWSGIServer((self.host, self.port), handler)
+    
+        # If cert variable is has a valid path, SSL will be used
+        # You can set it to None to disable SSL
+        cert = '/var/tmp/server.pem' # certificate path 
+        server.ssl_certificate = cert
+        server.ssl_private_key = cert
+        try:
+            server.start()
+        finally:
+            server.stop()
--- registrate.py
+++ registrate.py
@@ -1,13 +1,12 @@
 import sys, getopt
-from daos import *
-from models import microservice
-
+from app.models import microservice
+import rsa
 
 def main(argv):
     privatekey = ''
-    
+    name = ''
     try:
-        opts, args = getopt.getopt(argv, "hp:", ["privateKey="])
+        opts, args = getopt.getopt(argv, "n:k:", ["privateKey="])
     except getopt.GetoptError:
         print('runserver.py -p <privateKey>')
         sys.exit(2)  
@@ -15,8 +14,20 @@ def main(argv):
         if opt == '-h':
             print('runserver.py -p <privateKey>')
             sys.exit()
-        elif opt in ("-p", '--privateKey'):
-            privatekey = arg
+        elif opt in ("-n", '--name'):
+            name = arg
+        elif opt in ("-k", '--publicKey'):
+            publickey = arg
+    if publickey == '' or name == '':
+        print('registrate.py -n <name> -k <publicKey>')
+        sys.exit()
+    else:
+        with open(publickey, 'r') as keyfile:
+            keydata = keyfile.read()
+            print(keydata)
+        
+        pubkey = rsa.PublicKey.load_pkcs1_openssl_pem(keydata)
+        print(pubkey)
 
 if __name__ == '__main__':
     main(sys.argv[1:])
