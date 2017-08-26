5bc663a17acd9d8cd4e28e101bf4d0e5ae7877a0
Dennis Kleber <dennis_kleber@mailbox.org>
2017-08-26 22:13:10 +0200

    auth for crud

--- register/urls.py
+++ register/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^micro_add/$', views.add_microservice, name='add_microservice'),
+    url(r'micro_list/$', views.list_microservice, name='list_microservice')
 ]
 
 
--- ssh_server/ssh-server.py
+++ ssh_server/ssh-server.py
@@ -1,56 +1,36 @@
 import sys
 
+from binascii import hexlify
 import threading
 import paramiko
 import socket
 import traceback
 import gssapi
 from request_handler import request_handler
+from paramiko.py3compat import b, u, decodebytes
+
+
 host_key = paramiko.RSAKey(filename='../keys/sluice_rsa')
 #print(host_key)
-
+print('Read key: ' + u(hexlify(host_key.get_fingerprint())))
 class ssh_server(paramiko.ServerInterface):
 
     def __init__(self):
         self.event = threading.Event()
+    
+    def check_channel_request(self, kind, chanid):
+        if kind == 'session':
+            return paramiko.OPEN_SUCCEEDED
+        return paramiko.OPEN_FAILED_ADMINISTRATIVELY_PROHIBITED
 
     def check_auth_publickey(self, username, key):
-        publicKey = request_handler.get_publicKey(username)
-        print(publicKey)
-        if (key == publicKey):
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_SUCCESSFUL
-    def check_auth_gssapi_with_mic(self, username,
-                                   gss_authenticated=paramiko.AUTH_FAILED,
-                                   cc_file=None):
-        """
-        .. note::
-            We are just checking in `AuthHandler` that the given user is a
-            valid krb5 principal! We don't check if the krb5 principal is
-            allowed to log in on the server, because there is no way to do that
-            in python. So if you develop your own SSH server with paramiko for
-            a certain platform like Linux, you should call ``krb5_kuserok()`` in
-            your local kerberos library to make sure that the krb5_principal
-            has an account on the server and is allowed to log in as a user.
-        .. seealso::
-            `krb5_kuserok() man page
-            <http://www.unix.com/man-page/all/3/krb5_kuserok/>`_
-        """
-        if gss_authenticated == paramiko.AUTH_SUCCESSFUL:
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED
-
-    def check_auth_gssapi_keyex(self, username,
-                                gss_authenticated=paramiko.AUTH_FAILED,
-                                cc_file=None):
-        if gss_authenticated == paramiko.AUTH_SUCCESSFUL:
-            return paramiko.AUTH_SUCCESSFUL
-        return paramiko.AUTH_FAILED 
-
-
-    def enable_auth_gssapi(self):
-        return True
-
+        print('Auth attempt with key: ' + u(hexlify(key.get_fingerprint())))
+        if username == 'test':
+            publicKey = request_handler.get_publicKey(username)
+            print(publicKey)
+            if (key == publicKey):
+                return paramiko.AUTH_SUCCESSFUL
+        return paramiko.OPEN_FAILED_ADMINISTRATIVERY_PROHIBITED
 
     def check_channel_env_request(self, channel, name, value):
         #if name == 'INVALID_ENV':
@@ -91,8 +71,7 @@ except Exception as e:
 print('Got a connection!')
 
 try:
-    t = paramiko.Transport(client, gss_kex=DoGSSAPIKeyExchange)
-    t.set_gss_host(socket.getfqdn(""))
+    t = paramiko.Transport(client)
     try:
         t.load_server_moduli()
     except:
@@ -105,7 +84,7 @@ try:
     except paramiko.SSHException:
         print('*** SSH negotiation failed.')
         sys.exit(1)
-    chan = t.accept(20)
+    chan = t.accept(None)
     if chan is None:
         print('*** No channel.')
         sys.exit(1)
